f62b100a914c04e21a4983eaecef5cbbf6550a17
Ye-Yu-Mo <2908156919@qq.com>
2025-01-02 13:42:13 +0800

    语音识别

---
+++ common/asr.hpp
@@ -0,0 +1,30 @@
+#include "../third/aip-cpp-sdk/speech.h"
+#include "logger.hpp"
+
+namespace Common
+{
+    class ASRClient
+    {
+    public:
+        ASRClient(const std::string &appid,
+                  const std::string &api_key,
+                  const std::string &secret_key)
+            : _client(appid, api_key, secret_key)
+        {
+        }
+        std::string recognize(const std::string &speech_data)
+        {
+            Json::Value result = _client.recognize(speech_data, "pcm", 16000, aip::null);
+            if (result["err_no"].asInt() != 0)
+            {
+                log_error(logger, "失败, reason: %s", result["err_message"].asString().c_str());
+                return std::string();
+            }
+            info(logger, "语音识别成功!");
+            return result["result"][0].asString();
+        }
+
+    private:
+        aip::Speech _client;
+    };
+}

e7ae54aee4d6be173a6c062cdc09fe6fd744bdf9
Ye-Yu-Mo <2908156919@qq.com>
2025-01-03 11:26:23 +0800

    语音识别子服务

--- common/asr.hpp
+++ common/asr.hpp
@@ -6,13 +6,14 @@ namespace Common
     class ASRClient
     {
     public:
+        using ptr = std::shared_ptr<ASRClient>;
         ASRClient(const std::string &appid,
                   const std::string &api_key,
                   const std::string &secret_key)
             : _client(appid, api_key, secret_key)
         {
         }
-        std::string recognize(const std::string &speech_data)
+        std::string recognize(const std::string &speech_data, std::string& errmsg)
         {
             Json::Value result = _client.recognize(speech_data, "pcm", 16000, aip::null);
             if (result["err_no"].asInt() != 0)
